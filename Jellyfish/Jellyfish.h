958b2c5e0c111d54996d1053b736f215239b6d44
Krzysztof Cieplucha <krzysztof.cieplucha@gmail.com>
2015-09-07 12:55:19 +0200

    Setup project.

---
+++ Jellyfish/Jellyfish.h
@@ -0,0 +1,19 @@
+//
+//  Jellyfish.h
+//  Jellyfish
+//
+//  Created by Krzysztof Cieplucha on 07/07/15.
+//  Copyright (c) 2015 Krzysztof Cieplucha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jellyfish.
+FOUNDATION_EXPORT double JellyfishVersionNumber;
+
+//! Project version string for Jellyfish.
+FOUNDATION_EXPORT const unsigned char JellyfishVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jellyfish/PublicHeader.h>
+
+
